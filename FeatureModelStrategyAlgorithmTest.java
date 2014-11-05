48f73afd3519ec12728d3a0357120e804652fc52
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2014-11-04 21:20:26 -0500

    done 1

--- FeatureModelStrategyAlgorithmTest.java
+++ FeatureModelStrategyAlgorithmTest.java
@@ -2,13 +2,26 @@ package seg.jUCMNav.tests;
 
 import junit.framework.TestCase;
 import static org.junit.Assert.*;
+import fm.FeatureModel;
+import grl.EvaluationStrategy;
+import grl.GrlFactory;
+import grl.impl.EvaluationStrategyImpl;
+import grl.impl.GRLspecImpl;
+import grl.impl.GrlFactoryImpl;
+import grl.GRLspec;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert.*;
+
+import seg.jUCMNav.extensionpoints.IGRLStrategyAlgorithm;
+import seg.jUCMNav.strategies.FeatureModelStrategyAlgorithm;
 
 public class FeatureModelStrategyAlgorithmTest extends TestCase{
-
+	EvaluationStrategy strategy;
+	FeatureModelStrategyAlgorithm algo;
+	
 	public FeatureModelStrategyAlgorithmTest() {
 		// TODO Auto-generated constructor stub
 	}
@@ -17,16 +30,25 @@ public class FeatureModelStrategyAlgorithmTest extends TestCase{
 	protected void setUp() throws Exception {
 		// TODO Auto-generated method stub
 		super.setUp();
+		GrlFactory factory = GrlFactoryImpl.init();
+		strategy = factory.createEvaluationStrategy();
+		GRLspec spec = factory.createGRLspec();
+		strategy.setGrlspec(spec);
+		algo = new FeatureModelStrategyAlgorithm();		
+		
 	}
 	
 	@After
 	protected void tearDown() throws Exception {
 		// TODO Auto-generated method stub
 		super.tearDown();
+		algo = null;
+		strategy = null;
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetEvaluationType() {
+		int v = algo.getEvaluationType();
+		assertEquals(v,IGRLStrategyAlgorithm.EVAL_FEATURE_MODEL);
 	}
 }
