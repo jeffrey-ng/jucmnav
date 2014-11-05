105d9fc03386b51ec493d55100f59957d3bf220c
Jeffrey Ng <jeffrey.jn.ng@gmail.com>
2014-11-04 19:58:21 -0500

    first commit

---
+++ FeatureModelStrategyAlgorithmTest.java
@@ -0,0 +1,32 @@
+package seg.jUCMNav.tests;
+
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FeatureModelStrategyAlgorithmTest extends TestCase{
+
+	public FeatureModelStrategyAlgorithmTest() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Before
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+	
+	@After
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
